e6415d2194289c9c0d0cfb3ccf0bf829191b7076
Rohan Rustagi <110477025+RohanRusta21@users.noreply.github.com>
2023-08-16 18:13:18 +0000

    "modiji"

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!79n+mp+d)1-=z7w5ee9f5*74mhqbene^c*89j@(1gsot5&t@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com','https://*.127.0.0.1']
+
+ALLOWED_HOSTS = ['https://127.0.0.1:8000', 'localhost']
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/rooms/'
